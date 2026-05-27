fb4776e697a02127d05fb17bd948c34977d811e8
Varvara168 <v2004565@gmail.com>
2026-05-27 23:25:00 +0400

    hero section update

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,3 +1,5 @@
+import heroImage from "../assets/hero.png"
+
 export default function Hero() {
   return (
     <section className="relative h-screen w-full overflow-hidden bg-black text-white">
@@ -5,31 +7,31 @@ export default function Hero() {
       {/* ФОН */}
       <div className="absolute inset-0">
         <img
-          src="https://images.unsplash.com/photo-1500375592092-40eb2168fd21"
+          src={heroImage}
           className="h-full w-full object-cover opacity-60 scale-110"
         />
         <div className="absolute inset-0 bg-black/40" />
       </div>
 
       {/* ТЕКСТ */}
-      <div className="relative z-10 flex h-full flex-col items-center justify-center text-center px-6">
+      <div className="relative z-10 flex h-full flex-col items-left justify-center text-left px-6">
 
-        <p className="tracking-[0.3em] text-white/60 uppercase">
-          Premium Retreat
-        </p>
 
         <h1 className="mt-6 text-5xl md:text-7xl font-light leading-tight">
-          A place where <br />
-          time slows down
+          Приватный отдых <br />
+          в живописных регионах России 
         </h1>
+        
+
 
         <p className="mt-6 max-w-xl text-white/70">
-          Luxury nature experience with private villas, spa zones and exclusive comfort.
+          Пространство для восстановления себя. <br />
+          Новый опыт созданный для вас
         </p>
 
-        <button className="mt-10 px-8 py-3 bg-white text-black hover:scale-105 transition">
+        {/* <button className="mt-10 px-8 py-3 bg-white text-black hover:scale-105 transition">
           Explore
-        </button>
+        </button> */}
       </div>
 
       {/* НИЗ (декор) */}
